c7071cba5b68988f43ea622afe5c30e107fadca9
Israel M. Rios <israelm.riosjr@gmail.com>
2021-11-09 01:55:25 -0800

    Cleaned up code

--- db/index.js
+++ db/index.js
@@ -5,51 +5,33 @@ class Business {
         this.connection = connection;
     }
 
-    //should return employee id, first_name, last_name, role, department, salary, manager full name
-    findAllEmployees() {
-        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;");
-    }
-
-    newEmployee(data) {
-        return this.connection.promise().query('INSERT INTO employee SET ?', data);
-    }
-
-    employeeAndRole() {
-        return this.connection.promise().query("SELECT role.title, employee.* FROM employee LEFT JOIN role ON role.id = employee.role_id;")
-    }
-
-    changeEmployeeRole(eid, rid) {
-        return this.connection.promise().query('UPDATE employee SET role_id =? WHERE employee.id =?;', [rid, eid])
-    }
-
-    getAllRoles() {
-        return this.connection.promise().query("SELECT role.title, role.id, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;")
-    }
-
-    getJustDepartment() {
+    findAllDepartments() {
         return this.connection.promise().query("SELECT name, id FROM department;")
     }
 
-    newRole(data) {
-        return this.connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES ("${data.title}", "${data.salary}", ${data.department});`)
+    findAllRoles() {
+        return this.connection.promise().query("SELECT role.title, role.id, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;")
+    }
+
+    findAllEmployees() {
+        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;");
     }
 
     newDepartment(data) {
         return this.connection.promise().query(`INSERT INTO department(id, name) VALUES (id, "${data.department}");`)
     }
 
-    // getEmployees(){
-    //     return this.connection.promise().query("SELECT role.title, role.id, department.name AS department_name FROM role JOIN department ON role.department_id = department.id;")
-    // }
+    newRole(data) {
+        return this.connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES ("${data.title}", "${data.salary}", ${data.department});`)
+    }
 
-    // getEmployees(){
-    //     return this.connection.promise().query("SELECT id, first_name, employee.last_name FROM employee;");
-    // }
+    newEmployee(data) {
+        return this.connection.promise().query('INSERT INTO employee SET ?', data);
+    }
 
-    // getRoles(){
-    //     return this.connection.promise().query("SELECT id, title FROM role;")
-    // }
-
+    changeEmployeeRole(eid, rid) {
+        return this.connection.promise().query('UPDATE employee SET role_id =? WHERE employee.id =?;', [rid, eid])
+    }
 }
 
 module.exports = new Business(connection);
--- index.js
+++ index.js
@@ -14,16 +14,28 @@ function initialPrompt() {
     inquirer.prompt([
         {
             type: "list",
-            name: "whatToDo",
+            name: "options",
             message: "What would you like to do?",
-            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"]
+            choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "Quit"]
         }
     ]).then((data) => {
-        let choice = data.whatToDo;
+        let choice = data.options;
         console.log(choice)
         switch (choice) {
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
             case "View All Employees":
                 viewAllEmployees();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
                 break;
             case "Add Employee":
                 addEmployee();
@@ -31,175 +43,28 @@ function initialPrompt() {
             case "Update Employee Role":
                 updateEmployeeRole();
                 break;
-            case "View All Roles":
-                viewAllRoles();
-                break;
-            case "Add Role":
-                addRole();
-                break;
-            case "View All Departments":
-                viewAllDepartments();
-                break;
-            case "Add Department":
-                addDepartment();
-                break;
-            case "Quit":
-                process.exit()
             default:
                 process.exit()
         }
     })
 }
 
-function viewAllEmployees() {
-    db.findAllEmployees().then(([data]) => {
+function viewAllDepartments() {
+    db.findAllDepartments().then(([data]) => {
         console.log('\n')
         console.table(data)
     }).then(() => initialPrompt());
 }
 
-function addEmployee() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "first_name",
-            message: "Please enter employees First Name?"
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "Please enter employees Last Name?"
-        },
-    ]).then((data) => {
-        let empFirst = data.first_name;
-        let empLast = data.last_name;
-
-        db.getAllRoles().then(([data]) => {
-
-            const roleOptions = data.map(({ id, title }) => ({
-                name: title,
-                value: id
-            }))
-
-            inquirer.prompt([{
-                type: "list",
-                name: "role",
-                message: "Please enter employees Role?",
-                choices: roleOptions
-            }]).then(res => {
-                let roleId = res.role;
-
-                db.findAllEmployees().then(([data]) => {
-                    const managerOptions = data.map(({ id, first_name, last_name }) => ({
-                        name: `${first_name} ${last_name}`,
-                        value: id
-                    }));
-
-                    inquirer.prompt([{
-                        type: "list",
-                        name: "manager",
-                        message: "Please enter employees Manager?",
-                        choices: managerOptions
-                    }]).then(res => {
-                        let newEmployee = {
-                            manager_id: res.manager,
-                            role_id: roleId,
-                            first_name: empFirst,
-                            last_name: empLast
-                        }
-
-                        db.newEmployee(newEmployee);
-                    }).then(() => console.log(`added ${empFirst} ${empLast}`)).then(() => initialPrompt())
-                })
-            })
-        })
-    });
-
-
-
-
-
-}
-
-function updateEmployeeRole() {
-    db.findAllEmployees().then(([data]) => {
-
-
-        const names = data.map(({ id, first_name, last_name }) => ({
-            name: `${first_name} ${last_name}`,
-            value: id
-        }));
-
-        inquirer.prompt([
-            {
-                type: "list",
-                name: "name",
-                message: "Which employee would you like to change",
-                choices: names,
-            },
-
-        ]).then((data) => {
-            let empId = data.name;
-            db.getAllRoles().then(([data]) => {
-
-                const roleOptions = data.map(({ id, title }) => ({
-                    name: title,
-                    value: id
-                }));
-
-                inquirer.prompt([{
-                    type: "list",
-                    name: "roleId",
-                    message: "What is the new role for the employee? ",
-                    choices: roleOptions,
-                }]).then(res => db.changeEmployeeRole(empId, res.roleId)).then(() => console.log('updated the role')).then(() => initialPrompt())
-            })
-        })
-    })
-}
-
 function viewAllRoles() {
-    db.getAllRoles().then(([data]) => {
+    db.findAllRoles().then(([data]) => {
+        console.log('\n')
         console.table(data)
     }).then(() => initialPrompt());
 }
 
-function addRole() {
-    db.getJustDepartment().then(([data]) => {
-        const department = data.map((dept) => {
-            return { name: dept.name, value: dept.id };
-        });
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "title",
-                message: "Please enter the New Role?"
-            },
-            {
-                type: "number",
-                name: "salary",
-                message: "Please enter the salary for this role?"
-            },
-            {
-                type: "list",
-                name: "department",
-                message: "Please select which department this role belongs to?",
-                choices: department,
-            },
-        ]).then((data) => {
-            db.newRole(data).then(() => {
-                console.log(`\n You just added ${data.title} with a salary of ${data.salary} to the database. \n`);
-                initialPrompt();
-            })
-        })
-
-
-    })
-
-}
-
-function viewAllDepartments() {
-    db.getJustDepartment().then(([data]) => {
+function viewAllEmployees() {
+    db.findAllEmployees().then(([data]) => {
         console.log('\n')
         console.table(data)
     }).then(() => initialPrompt());
@@ -218,6 +83,140 @@ function addDepartment() {
             initialPrompt();
         })
     });
+}
+
+function addRole() {
+    db.findAllDepartments().then(([data]) => {
+        const department = data.map((dept) => {
+            return { name: dept.name, value: dept.id };
+        });
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Please enter the New Role Title?"
+            },
+            {
+                type: "number",
+                name: "salary",
+                message: "Please enter the salary for this role?"
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "Which department does this new role belongs to?",
+                choices: department
+            }
+        ]).then((data) => {
+            db.newRole(data).then(() => {
+                console.log(`\n You just added ${data.title} with a salary of ${data.salary} to the database. \n`);
+                initialPrompt();
+            })
+        })
+    })
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "Please enter employees First Name?"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "Please enter employees Last Name?"
+        }
+    ]).then((data) => {
+        let empFirst = data.first_name;
+        let empLast = data.last_name;
+
+        db.findAllRoles().then(([data]) => {
+
+            const roleOptions = data.map(({ id, title }) => ({
+                name: title,
+                value: id
+            }))
+
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "role",
+                    message: "Please enter employees Role?",
+                    choices: roleOptions
+                }
+            ]).then(res => {
+                let roleId = res.role;
+
+                db.findAllEmployees().then(([data]) => {
+                    const managerOptions = data.map(({ id, first_name, last_name }) => ({
+                        name: `${first_name} ${last_name}`,
+                        value: id
+                    }));
+
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "manager",
+                            message: "Please enter employees Manager?",
+                            choices: managerOptions
+                        }
+                    ]).then(res => {
+                        let newEmployee = {
+                            manager_id: res.manager,
+                            role_id: roleId,
+                            first_name: empFirst,
+                            last_name: empLast
+                        }
+                        db.newEmployee(newEmployee);
+                    }).then(() => {
+                        console.log(`\n You just added ${empFirst} ${empLast} to database. \n`);
+                        initialPrompt();
+                    })
+                })
+            })
+        })
+    });
+}
+
+function updateEmployeeRole() {
+    db.findAllEmployees().then(([data]) => {
+        const names = data.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "name",
+                message: "Which employee would you like to update?",
+                choices: names
+            }
+        ]).then((data) => {
+            let empId = data.name;
+            db.findAllRoles().then(([data]) => {
+
+                const roleOptions = data.map(({ id, title }) => ({
+                    name: title,
+                    value: id
+                }));
+
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "roleId",
+                        message: "What is this employees new role/title? ",
+                        choices: roleOptions
+                    }
+                ]).then(res => db.changeEmployeeRole(empId, res.roleId)).then(() => {
+                    console.log(`\n This employee was just updated in the database. \n`);
+                    initialPrompt();
+                })
+            })
+        })
+    })
 }
 
 init();
